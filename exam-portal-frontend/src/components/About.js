f358fcd8d36b7a995846be609c7590c60e01db0d
Jai Tembhare <jaitembhare@gmail.com>
2023-08-25 19:41:34 +0530

    Homepage Added

--- exam-portal-frontend/src/components/About.js
+++ exam-portal-frontend/src/components/About.js
@@ -2,15 +2,19 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import imageSrc from './images/jai.jpg';
 import imagePawan from './images/Pawan.jpeg';
+import imageRuchika from './images/Ruchika.png';
+import imageTejas from './images/Tejas.jpeg';
+import imageMudassir from './images/Mudassir.jpg';
 import '../components/css/About.css';
 import { FaEnvelope, FaInstagram, FaLinkedin, FaPhone } from 'react-icons/fa';
 import Footer from '../components/Footer.js';
 
 export default function About() {
   return (
+    
     <React.Fragment>
        <section className="about-section">
-        <div className="container">
+        <div className="container-about">
           <h1>About Us</h1>
           <p className="lead">
             We are here to revolutionize the way exams are conducted and assessed. Our Online Examination System offers a comprehensive, efficient, and modern approach to online examinations.
@@ -18,7 +22,7 @@ export default function About() {
         </div>
       </section>
       <section className="features-section">
-      <div className="container">
+      <div className="container-about">
         <h2 className="section-title text-white">Our Features</h2>
         <div className="row justify-content-center">
           <div className="col-md-5 feature">
@@ -43,23 +47,23 @@ export default function About() {
     </section>
       
 
-      <div className="container">
+      <div className="container-about ">
         <h2 style={{ textAlign: 'center' }}>Our Team</h2>
         <div className="row justify-content-between">
           {/* Team members */}
           <div className="col-md-4">
-            <div className="card">
+            <div className="card-about">
               <img
                 src={imageSrc}
-                className="card-img-top rounded-circle border"
+                className="card-img-top rounded-circle border ali"
                 alt="Card"
                 style={{ width: '200px', height: '200px', margin: 'auto' }}
               />
-              <div className="container">
+              <div className="container-about">
                 <h2>Jai Tembhare</h2>
-                <p className="title">Frontend Devloper</p>
+                <p className="title-about">Frontend Devloper</p>
                 <p><FaEnvelope /> jaitembhare@gmail.com</p>
-                <p><FaPhone /> +917721887151</p>
+         
                 <div className="social-icons">
                     <a href="https://instagram.com/jai_tembhare?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D"><FaInstagram /></a>
                     <a href="https://www.linkedin.com/in/jai-tembhare-1b4665204"><FaLinkedin /></a>
@@ -68,63 +72,83 @@ export default function About() {
             </div>
           </div>
           <div className="col-md-4">
-            <div className="card">
+            <div className="card-about">
               <img
-                src={imagePawan}
-                className="card-img-top rounded-circle border"
+                src={imageRuchika}
+                className="card-img-top rounded-circle border "
                 alt="Card"
                 style={{ width: '200px', height: '200px', margin: 'auto' }}
               />
-              <div className="container">
-                <h2>Pawan Ramtake</h2>
-                <p className="title">Backend Devloper</p>
-                <p><FaEnvelope /> pawanramtake98@gmail.com</p>
-                <p><FaPhone />+917774997165</p>
+              <div className="container-about">
+                <h2>Ruchika Gaidhani</h2>
+                <p className="title-about">Database Administrator</p>
+                <p><FaEnvelope /> ruchikagaidhani19@gmail.com</p>
+              
                 <div className="social-icons">
-                    <a href="#"><FaInstagram /></a>
-                    <a href="#"><FaLinkedin /></a>
+                    <a href=" https://www.instagram.com/r_u_c_h19/?next=%2F"><FaInstagram /></a>
+                    <a href="https://www.linkedin.com/in/ruchika-gaidhani-670972199/"><FaLinkedin /></a>
                 </div>
               </div>
             </div>
           </div>
           <div className="col-md-4">
-            <div className="card">
+            <div className="card-about">
               <img
-                src={imagePawan}
+                src={imageTejas}
                 className="card-img-top rounded-circle border"
                 alt="Card"
                 style={{ width: '200px', height: '200px', margin: 'auto' }}
               />
-              <div className="container">
-                <h2>Pawan Ramtake</h2>
-                <p className="title">Backend Devloper</p>
-                <p><FaEnvelope /> pawanramtake98@gmail.com</p>
-                <p><FaPhone />+917774997165</p>
+              <div className="container-about">
+                <h2> Tejas Kolambe</h2>
+                <p className="title-about">Backend Devloper</p>
+                <p><FaEnvelope /> kolambetejas79@gmail.com</p>
+                
                 <div className="social-icons">
-                    <a href="#"><FaInstagram /></a>
-                    <a href="#"><FaLinkedin /></a>
+                    <a href="https://www.instagram.com/tejaskolambe/"><FaInstagram /></a>
+                    <a href="www.linkedin.com/in/tejas-kolambe-2a0310209"><FaLinkedin /></a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="container">
+        <div className="container-about">
         <h2 style={{ textAlign: 'center' }}></h2>
         <div className="row justify-content-between">
           {/* Team members */}
           <div className="col-md-4">
-            <div className="card">
+            <div className="card-about">
+              <img
+                src={imageMudassir}
+                className="card-img-top rounded-circle border"
+                alt="Card-about"
+                style={{ width: '200px', height: '200px', margin: 'auto' }}
+              />
+              <div className="container-about">
+                <h2>Momin Mudassir</h2>
+                <p className="title-about">Backend Devloper</p>
+                <p><FaEnvelope /> Mudassirraza1286@gmail.com</p>
+          
+                <div className="social-icons">
+                    <a href="https://www.instagram.com/mudassirraza1286/"><FaInstagram /></a>
+                    <a href="http://www.linkedin.com/in/mudassir-raza-14bb15289"><FaLinkedin /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card-about">
               <img
                 src={imagePawan}
                 className="card-img-top rounded-circle border"
                 alt="Card"
                 style={{ width: '200px', height: '200px', margin: 'auto' }}
               />
-              <div className="container">
+              <div className="container-about">
                 <h2>Pawan Ramtake</h2>
-                <p className="title">Backend Devloper</p>
+                <p className="title-about">Frontend Devloper</p>
                 <p><FaEnvelope /> pawanramtake98@gmail.com</p>
-                <p><FaPhone />+917774997165</p>
+               
                 <div className="social-icons">
                     <a href="#"><FaInstagram /></a>
                     <a href="#"><FaLinkedin /></a>
@@ -133,38 +157,18 @@ export default function About() {
             </div>
           </div>
           <div className="col-md-4">
-            <div className="card">
+            <div className="card-about">
               <img
                 src={imagePawan}
                 className="card-img-top rounded-circle border"
                 alt="Card"
                 style={{ width: '200px', height: '200px', margin: 'auto' }}
               />
-              <div className="container">
+              <div className="container-about">
                 <h2>Pawan Ramtake</h2>
-                <p className="title">Backend Devloper</p>
+                <p className="title-about">Backend Devloper</p>
                 <p><FaEnvelope /> pawanramtake98@gmail.com</p>
-                <p><FaPhone />+917774997165</p>
-                <div className="social-icons">
-                    <a href="#"><FaInstagram /></a>
-                    <a href="#"><FaLinkedin /></a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card">
-              <img
-                src={imagePawan}
-                className="card-img-top rounded-circle border"
-                alt="Card"
-                style={{ width: '200px', height: '200px', margin: 'auto' }}
-              />
-              <div className="container">
-                <h2>Pawan Ramtake</h2>
-                <p className="title">Backend Devloper</p>
-                <p><FaEnvelope /> pawanramtake98@gmail.com</p>
-                <p><FaPhone />+917774997165</p>
+       
                 <div className="social-icons">
                     <a href="#"><FaInstagram /></a>
                     <a href="#"><FaLinkedin /></a>
